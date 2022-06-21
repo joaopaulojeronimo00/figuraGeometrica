558125eab75bbf6e84dfbd8fae05cde0b205265a
marialago <maria.lago@radixeng.com.br>
2022-06-21 14:48:47 -0300

    update from drive

--- header/PutEllipsoid.h
+++ header/PutEllipsoid.h
@@ -9,7 +9,7 @@ protected:
 
 
 public:
-PutEllipsoid(int xc, int yc, int zc, int radius_x, int radius_y, int radius_z, int r, int g, int b, int a)
+PutEllipsoid(int xc, int yc, int zc, int radius_x, int radius_y, int radius_z, int r, int g, int b, int a);
 ~PutEllipsoid() {};
 	void draw(Sculptor& t);
 };
